b19b10d18647bb3cc0df39d958df716d66048155
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-09-14 17:50:57 -0500

    Getting a basic crawl working

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 "use strict";
-console.log('test');
+
+// Importing the frameworks we need
+var rp = require('request-promise');
+var cheerio = require('cheerio');
+
+// Running code
+var options = {
+    uri: 'https://uwosh.edu',
+    transform: function (body) {
+        return cheerio.load(body);
+    }
+};
+
+rp(options)
+    .then(function ($) {
+        // Process html like you would with jQuery...
+        console.log($('.site-title h1 a').text().trim());
+    })
+    .catch(function (err) {
+        // Crawling failed or Cheerio choked...
+        console.log("Error: " + err);
+    });
