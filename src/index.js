d2681bce04d9f5c1ab83234331a636fcdca83265
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-10-17 17:47:33 -0500

    getting titles of all stories to fetch properly

--- src/index.js
+++ src/index.js
@@ -1,32 +1,76 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-let scrapeSitemap = async (site) => {
-    // setup
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    const sitemap = site + 'sitemap.xml'
-    await page.goto(sitemap);
+// goes to the site specified and grabs an array of news URLs that matches "campus/news/releases"
+let fetchNewsURLs = async (browser, site) => {
+  // setup
+  const page = await browser.newPage();
+  const sitemap = site + "sitemap.xml";
+  await page.goto(sitemap);
+
+  // begin scraping
+  const allLinks = await page.evaluate(site => {
+    let all = Array.from(document.querySelectorAll("tr > td > a")).map(
+      element => element.href
+    );
     let links = []; // the array we will store all our relevant site links inside
-
-    // begin scraping
-    const allLinks = await page.evaluate(() => {
-        return document.querySelector('tr td a').innerText;
+    all.forEach(element => {
+      if (element.includes(site + "campus/news/releases")) {
+        links.push(element);
+      }
     });
+    return links;
+  }, site);
 
-    // return scrape
-    browser.close();
-    return allLinks;
-}
+  // return scrape
+  return allLinks;
+};
 
-let fdl_url = "http://fdl.uwc.edu/";
-let fox_url = "http://uwfox.uwc.edu/";
+let scrapeNewsArticleData = async (browser, url) => {
+  const page = await browser.newPage();
+  await page.goto(url);
 
-scrapeSitemap(fdl_url).then((response) => {
-    console.log("fdl: " + response);
-});
+  const articleData = await page.evaluate(() => {
+    let title;
 
-scrapeSitemap(fox_url).then((response) => {
-    console.log("fox: " + response);
-});
+    // get title
+    title = document.querySelector("#page-title > span").innerText;
+
+    return {
+      title
+    };
+  });
+
+  return articleData;
+};
+
+let main = async () => {
+  // setup
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+
+  let fdlSiteURL = "http://fdl.uwc.edu/";
+  let foxSiteURL = "http://uwfox.uwc.edu/";
+
+  let foxNewsURLs = await fetchNewsURLs(browser, foxSiteURL);
+  let fdlNewsURLs = await fetchNewsURLs(browser, fdlSiteURL);
+
+  let foxStories = [];
+  for (let i = 0; i < foxNewsURLs.length; i++) {
+    let story = await scrapeNewsArticleData(browser, foxNewsURLs[i]);
+    foxStories.push(story);
+  }
+
+  let fdlStories = [];
+  for (let i = 0; i < fdlNewsURLs.length; i++) {
+    let story = await scrapeNewsArticleData(browser, fdlNewsURLs[i]);
+    fdlStories.push(story);
+  }
+
+  console.log("fox[0] story:" + JSON.stringify(foxStories[0]));
+  console.log("fdl[0] story: " + JSON.stringify(fdlStories[0]));
+
+  browser.close();
+};
+
+main();
