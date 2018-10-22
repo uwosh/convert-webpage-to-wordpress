f6a49156b48421f70e8cd40bbe6eb1cef1c8cac1
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-10-22 15:07:31 -0500

    getting the wp cli info working

--- src/WP-CLI.js
+++ src/WP-CLI.js
@@ -1,9 +1,9 @@
 // importing npm packages
 const exec = require('child_process').exec;
-
+const _WP_CLI = "php lib/wp-cli.phar ";
 export default class WPCLI {
     constructor() {
-        exec("ls -la", this.log); // sample shell command
+        exec(_WP_CLI + "--info", this.log); // sample shell command
     }
 
     // function that logs output from shell execution
