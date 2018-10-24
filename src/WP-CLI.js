60dc50252f1d643a850769e642e6049cde1a2125
Kerkhof, Joseph A <musicaljoeker@gmail.com>
2018-10-24 15:44:42 -0500

    importMedia function working on remote site

--- src/WP-CLI.js
+++ src/WP-CLI.js
@@ -17,7 +17,24 @@ class WPCLI {
         let targetSite = _WP_TEST_SITE + _SITE;
 
         let siteID = this.getSiteID(targetSSH, targetSite);
-        console.log("siteID: " + siteID);
+
+        let newURL = this.importMedia(targetSSH, targetSite, "http://uwfox.uwc.edu/sites/uwfox.uwc.edu/files/resize/imce-uploads/campus/_images/fox-oustanding-alumni-2016-web-297x198.jpg");
+        console.log(newURL);
+    }
+
+    // imports media to the site you set
+    importMedia(env, site, mediaURL) {
+        let importMediaCommand = _WP_CLI + env + "--url=" + site + " media import " + mediaURL;
+        let responseMediaCommand = execSync(importMediaCommand).toString();
+
+        // parse out attachment ID
+        let regex = /(?<=attachment\sID\s)([0-9]*)/;
+        let mediaID = responseMediaCommand.match(regex)[0];
+
+        // TODO: fetch URL of new media
+        let fetchMediaURL = _WP_CLI + env + "--url=" + site + " post get " + mediaID + " --field=guid";
+        let responseMediaURL = execSync(fetchMediaURL).toString();
+        return responseMediaURL;
     }
 
     getSiteID(env, site) {
